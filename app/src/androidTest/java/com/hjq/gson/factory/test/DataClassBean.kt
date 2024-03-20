53071ce65d24ff135cb68f296184a62d28355f0a
轮子哥 <jinqun0730@gmail.com>
2024-03-20 12:20:01 +0800

    优化单元测试用例中用于测试 Bean 类字段的排序

--- app/src/androidTest/java/com/hjq/gson/factory/test/DataClassBean.kt
+++ app/src/androidTest/java/com/hjq/gson/factory/test/DataClassBean.kt
@@ -1,13 +1,13 @@
 package com.hjq.gson.factory.test
 
 data class DataClassBean(
-    var name: String = "轮子哥",
-    var alias: String,
     var address: String? = "",
-    var company: String?,
     var age: Int = 20,
-    var weight: Int,
-    var stature: Int? = 180,
+    var alias: String,
+    var child: DataClassChildBean,
+    var company: String?,
     var interest: List<String>,
-    var child: DataClassChildBean
+    var name: String = "轮子哥",
+    var stature: Int? = 180,
+    var weight: Int
 )
--- app/src/androidTest/java/com/hjq/gson/factory/test/JsonBean.java
+++ app/src/androidTest/java/com/hjq/gson/factory/test/JsonBean.java
@@ -14,11 +14,12 @@ import org.json.JSONObject;
  */
 public final class JsonBean {
 
-    private List<String> listTest1;
-    private List<Double> listTest2;
-    private List<Integer> listTest3;
-    private List<Boolean> listTest4;
-    private List<Double> listTest5;
+    private TestBean bean1;
+    private TestBean bean2;
+
+    private BigDecimal bigDecimal1;
+    private BigDecimal bigDecimal2;
+    private BigDecimal bigDecimal3;
 
     private boolean booleanTest1;
     private boolean booleanTest2;
@@ -27,23 +28,11 @@ public final class JsonBean {
     private boolean booleanTest5;
     private boolean booleanTest6;
 
-    private String stringTest1;
-    private String stringTest2;
-    private String stringTest3;
-    private String stringTest4;
-    private String stringTest5;
-
-    private int intTest1;
-    private int intTest2;
-    private int intTest3;
-    private int intTest4;
-    private int intTest5;
-
-    private long longTest1;
-    private long longTest2;
-    private long longTest3;
-    private long longTest4;
-    private long longTest5;
+    private double doubleTest1;
+    private double doubleTest2;
+    private double doubleTest3;
+    private double doubleTest4;
+    private double doubleTest5;
 
     private float floatTest1;
     private float floatTest2;
@@ -51,26 +40,37 @@ public final class JsonBean {
     private float floatTest4;
     private float floatTest5;
 
-    private double doubleTest1;
-    private double doubleTest2;
-    private double doubleTest3;
-    private double doubleTest4;
-    private double doubleTest5;
+    private int intTest1;
+    private int intTest2;
+    private int intTest3;
+    private int intTest4;
+    private int intTest5;
 
-    private BigDecimal bigDecimal1;
-    private BigDecimal bigDecimal2;
-    private BigDecimal bigDecimal3;
+    private JSONObject jsonObject;
+    private JSONArray jsonArray;
 
-    private TestBean bean1;
-    private TestBean bean2;
+    private List<String> listTest1;
+    private List<Double> listTest2;
+    private List<Integer> listTest3;
+    private List<Boolean> listTest4;
+    private List<Double> listTest5;
+
+    private long longTest1;
+    private long longTest2;
+    private long longTest3;
+    private long longTest4;
+    private long longTest5;
 
     private Map<String, Boolean> map1;
     private Map<String, Integer> map2;
     private Map<String, Integer> map3;
     private Map<String, Integer> map4;
 
-    private JSONObject jsonObject;
-    private JSONArray jsonArray;
+    private String stringTest1;
+    private String stringTest2;
+    private String stringTest3;
+    private String stringTest4;
+    private String stringTest5;
 
     private static class TestBean {
 
